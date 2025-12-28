6081d1b65947be90146efaeef4e326586f83a9d3
Noone-dosve <dosve@DESKTOP-355J56P.localdomain>
2025-12-28 15:35:24 +0800

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,3 @@
+This is a learning project of https://github.com/pingcap/talent-plan/tree/master/courses/rust/projects
+
+I develop this in WSL, Ubuntu 20.04.
---
+++ src/lib.rs
@@ -0,0 +1,22 @@
+pub struct KvStore;
+
+impl KvStore {
+    pub fn new() -> Self {
+        KvStore
+    }
+}
+
+pub fn add(left: u64, right: u64) -> u64 {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
