99d3a263681475f350f7efeed8e1dbbd624bbee4
ReDosve <dosve1999@gmail.com>
2025-12-29 20:22:06 +0800

    FEAT: pass all the cli_* testr

--- src/bin/kvs.rs
+++ src/bin/kvs.rs
@@ -1,3 +1,35 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    commands: Vec<String>,
+}
+
 fn main() {
+    let args = Args::parse();
+
+    if args.commands.len() == 0 {
+        panic!()
+    } else {
+        // means there is a command
+        let action = args.commands[0].clone();
+
+        if action == "get" {
+            // get must have ONE value
+            panic!("unimplemented")
+        } else if action == "set" {
+            panic!("unimplemented")
+        } else if action == "rm" {
+            panic!("unimplemented")
+        } else {
+            panic!("unknown subcommand")
+        }
+    }
+
+    for (_, command) in args.commands.iter().enumerate() {
+        println!("command: {}", command);
+    }
+
     println!("Hello, world!");
 }
