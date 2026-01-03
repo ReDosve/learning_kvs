177fb5a5f3b96e9e9e0b5cd88261c9f4fad665c2
ReDosve <dosve1999@gmail.com>
2026-01-03 14:41:22 +0800

    FEAT: pass cargo test --doc

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,30 @@
+//! Simple KVStore
+
+#![deny(missing_docs)]
 use std::collections::HashMap;
 
+/// A simple KVStore based on memory
+/// 'KVStore' use hashmap to sotre key-value pair
+///
+/// # example
+///
+/// ```
+/// use kvs::KvStore;
+///
+/// let mut store = KvStore::new();
+///
+/// ```
 pub struct KvStore {
     data: HashMap<String, String>,
 }
 
 impl KvStore {
+    /// ```
+    /// use kvs::KvStore;
+    ///
+    /// let mut store = KvStore::new();
+    /// assert_eq!(store.get("nonexistent".to_string()), None);
+    /// ```
     pub fn new() -> Self {
         let store = KvStore {
             data: HashMap::new(),
@@ -12,30 +32,42 @@ impl KvStore {
         store
     }
 
+    /// ```
+    /// use kvs::KvStore;
+    ///
+    /// let mut store = KvStore::new();
+    ///
+    /// assert_eq!(store.set("key1".to_string(), "value1".to_string()), ());
+    ///
+    /// assert_eq!(store.set("key1".to_string(), "new_value".to_string()),());
+    ///            
+    /// ```
     pub fn set(&mut self, key: String, value: String) {
         self.data.insert(key, value);
     }
 
+    /// ```
+    /// use kvs::KvStore;
+    ///
+    /// let mut store = KvStore::new();
+    /// store.set("key1".to_string(), "value1".to_string());
+    ///
+    /// assert_eq!(store.get("key1".to_string()), Some("value1".to_string()));
+    /// assert_eq!(store.get("nonexistent".to_string()), None);
+    /// ```
     pub fn get(&mut self, key: String) -> Option<String> {
         self.data.get(&key).cloned()
     }
 
+    /// ```
+    /// use kvs::KvStore;
+    ///
+    /// let mut store = KvStore::new();
+    /// store.set("key1".to_string(), "value1".to_string());
+    ///
+    /// assert_eq!(store.remove("nonexistent".to_string()), ());
+    /// ```
     pub fn remove(&mut self, key: String) {
         self.data.remove(&key);
-    }
-}
-
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
     }
 }
